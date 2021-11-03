5606fb74e4f52353303f98b081bb5f1e8f94d43c
Sven Jäger <sv_jaeger@gmx.de>
2021-11-03 22:22:15 +0100

    add rect buffer

--- libraries/LDisplay/LDisplay.cpp
+++ libraries/LDisplay/LDisplay.cpp
@@ -3,7 +3,6 @@
 #include "LDisplay.h"
 #include "Display.h"
 
-
 void LDisplayClass::init(void)
 {
 	remoteCall(lcd_init, NULL);
@@ -24,12 +23,12 @@ void LDisplayClass::screen_set(uint32_t ulValue)
 	remoteCall(lcd_screen_set, &ulValue);
 }
 
-void LDisplayClass::draw_font(uint8_t ulX, uint8_t ulY, const char* str, uint32_t BCulValue, uint32_t FCulValue)
+void LDisplayClass::draw_font(uint8_t ulX, uint8_t ulY, const char *str, uint32_t BCulValue, uint32_t FCulValue)
 {
 	string_info_struct data;
 	data.ulX = ulX;
 	data.ulY = ulY;
-	data.str = (VMCHAR*)str;
+	data.str = (VMCHAR *)str;
 	data.BCulValue = BCulValue;
 	data.FCulValue = FCulValue;
 	remoteCall(lcd_draw_font, &data);
@@ -37,7 +36,7 @@ void LDisplayClass::draw_font(uint8_t ulX, uint8_t ulY, const char* str, uint32_
 
 void LDisplayClass::draw_number(uint8_t ulX, uint8_t ulY, uint32_t ulData, uint32_t BCulValue, uint32_t FCulValue)
 {
-	number_info_struct data;	
+	number_info_struct data;
 	data.ulX = ulX;
 	data.ulY = ulY;
 	data.ulData = ulData;
@@ -48,7 +47,7 @@ void LDisplayClass::draw_number(uint8_t ulX, uint8_t ulY, uint32_t ulData, uint3
 
 void LDisplayClass::draw_point(uint8_t ulX, uint8_t ulY, uint32_t PCulValue)
 {
-	point_info_struct data;	
+	point_info_struct data;
 	data.ulX = ulX;
 	data.ulY = ulY;
 	data.PCulValue = PCulValue;
@@ -57,7 +56,7 @@ void LDisplayClass::draw_point(uint8_t ulX, uint8_t ulY, uint32_t PCulValue)
 
 void LDisplayClass::draw_line(uint8_t ulX1, uint8_t ulY1, uint8_t ulX2, uint8_t ulY2, uint32_t LCulValue)
 {
-	line_info_struct data;	
+	line_info_struct data;
 	data.ulX1 = ulX1;
 	data.ulY1 = ulY1;
 	data.ulX2 = ulX2;
@@ -68,7 +67,7 @@ void LDisplayClass::draw_line(uint8_t ulX1, uint8_t ulY1, uint8_t ulX2, uint8_t 
 
 void LDisplayClass::draw_fill_rectangle(uint8_t ulX1, uint8_t ulY1, uint8_t ulX2, uint8_t ulY2, uint32_t FCulValue)
 {
-	fill_info_struct data;	
+	fill_info_struct data;
 	data.ulX1 = ulX1;
 	data.ulY1 = ulY1;
 	data.ulX2 = ulX2;
@@ -80,6 +79,17 @@ void LDisplayClass::draw_fill_rectangle(uint8_t ulX1, uint8_t ulY1, uint8_t ulX2
 void LDisplayClass::draw_updata(void)
 {
 	remoteCall(lcd_draw_updata, NULL);
+}
+
+void LDisplayClass::draw_rect_buffer(uint8_t ulX1, uint8_t ulY1, uint8_t ulX2, uint8_t ulY2, const uint32_t *FCulValue)
+{
+	rect_buffer_struct data;
+	data.ulX1 = ulX1;
+	data.ulY1 = ulY1;
+	data.ulX2 = ulX2;
+	data.ulY2 = ulY2;
+	data.FCulValue = FCulValue;
+	remoteCall(lcd_draw_rect_buffer, &data);
 }
 
 LDisplayClass Lcd;
