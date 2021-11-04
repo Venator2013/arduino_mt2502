981bc1efbba56f7d4dd7e9e72e2733b13aba4784
Sven Jäger <sv_jaeger@gmx.de>
2021-11-04 20:44:30 +0100

    update LDisply

--- libraries/LDisplay/LDisplay.cpp
+++ libraries/LDisplay/LDisplay.cpp
@@ -3,6 +3,7 @@
 #include "LDisplay.h"
 #include "Display.h"
 
+
 void LDisplayClass::init(void)
 {
 	remoteCall(lcd_init, NULL);
@@ -23,31 +24,33 @@ void LDisplayClass::screen_set(uint32_t ulValue)
 	remoteCall(lcd_screen_set, &ulValue);
 }
 
-void LDisplayClass::draw_font(uint8_t ulX, uint8_t ulY, const char *str, uint32_t BCulValue, uint32_t FCulValue)
+void LDisplayClass::draw_font(uint8_t ulX, uint8_t ulY, const char* str, uint32_t BCulValue, uint32_t FCulValue, uint8_t ulSize)
 {
 	string_info_struct data;
 	data.ulX = ulX;
 	data.ulY = ulY;
-	data.str = (VMCHAR *)str;
+	data.str = (VMCHAR*)str;
 	data.BCulValue = BCulValue;
 	data.FCulValue = FCulValue;
+	data.ulSize = ulSize;
 	remoteCall(lcd_draw_font, &data);
 }
 
-void LDisplayClass::draw_number(uint8_t ulX, uint8_t ulY, uint32_t ulData, uint32_t BCulValue, uint32_t FCulValue)
+void LDisplayClass::draw_number(uint8_t ulX, uint8_t ulY, uint32_t ulData, uint32_t BCulValue, uint32_t FCulValue, uint8_t ulSize)
 {
-	number_info_struct data;
+	number_info_struct data;	
 	data.ulX = ulX;
 	data.ulY = ulY;
 	data.ulData = ulData;
 	data.BCulValue = BCulValue;
 	data.FCulValue = FCulValue;
+	data.ulSize = ulSize;
 	remoteCall(lcd_draw_number, &data);
 }
 
 void LDisplayClass::draw_point(uint8_t ulX, uint8_t ulY, uint32_t PCulValue)
 {
-	point_info_struct data;
+	point_info_struct data;	
 	data.ulX = ulX;
 	data.ulY = ulY;
 	data.PCulValue = PCulValue;
@@ -56,7 +59,7 @@ void LDisplayClass::draw_point(uint8_t ulX, uint8_t ulY, uint32_t PCulValue)
 
 void LDisplayClass::draw_line(uint8_t ulX1, uint8_t ulY1, uint8_t ulX2, uint8_t ulY2, uint32_t LCulValue)
 {
-	line_info_struct data;
+	line_info_struct data;	
 	data.ulX1 = ulX1;
 	data.ulY1 = ulY1;
 	data.ulX2 = ulX2;
@@ -67,7 +70,7 @@ void LDisplayClass::draw_line(uint8_t ulX1, uint8_t ulY1, uint8_t ulX2, uint8_t 
 
 void LDisplayClass::draw_fill_rectangle(uint8_t ulX1, uint8_t ulY1, uint8_t ulX2, uint8_t ulY2, uint32_t FCulValue)
 {
-	fill_info_struct data;
+	fill_info_struct data;	
 	data.ulX1 = ulX1;
 	data.ulY1 = ulY1;
 	data.ulX2 = ulX2;
